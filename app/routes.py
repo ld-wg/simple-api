52c3a5e3235d62296a43e7917759664a341ecf2c
ld-wg <ludwig.aumann@gmail.com>
2024-06-14 19:02:35 -0300

    add event types req

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
-from flask import Flask
-from .services import fetch_user_data, create_user_data, get_balance, reset_user_data
+from flask import Flask, jsonify, request
+from .services import fetch_user_data, create_user_data, get_balance, handle_deposit, handle_transfer, handle_withdraw, reset_user_data
 
 def set_routes(app: Flask):
     @app.route('/')
     def home():
-        return 'Home'
+        return 'root'
 
     @app.route("/get-user/<user_id>")
     def get_user(user_id):
@@ -21,3 +21,17 @@ def set_routes(app: Flask):
     @app.route('/balance', methods=['GET'])
     def balance():
         return get_balance()
+    
+    @app.route('/event', methods=['POST'])
+    def handle_event():
+        data = request.get_json()
+        event_type = data.get('type')
+        
+        if event_type == 'deposit':
+            return handle_deposit(data)
+        if event_type == 'withdraw':
+            return handle_withdraw(data)
+        if event_type == 'transfer':
+            return handle_transfer(data)
+        else:
+            return jsonify({'error': 'Invalid event type'}), 400
--- app/services.py
+++ app/services.py
@@ -33,10 +33,55 @@ def get_balance():
     if account_id is None:
         return jsonify({'error': 'Account ID is required'}), 400
 
-    balance = get_account_balance(account_id)
+    balance = accounts.get(account_id)
+
     if balance is None:
         return jsonify(0), 404
+    
     return jsonify(balance), 200
 
-def get_account_balance(account_id):
-    return accounts.get(account_id)
+def handle_deposit(data):
+    destination = data.get('destination')
+    amount = data.get('amount')
+    
+    if destination not in accounts:
+        accounts[destination] = 0  # Create a new account if it doesn't exist
+    
+    accounts[destination] += amount  # Deposit the amount
+    return jsonify({'destination': {'id': destination, 'balance': accounts[destination]}}), 201
+
+def handle_withdraw(data):
+    origin = data.get('origin')
+    amount = data.get('amount')
+
+    if origin not in accounts:
+        return jsonify(0), 404
+    
+    accounts[origin] -= amount  # Withdraw the amount
+    return jsonify({"origin": {"id":origin, "balance": accounts[origin]}})
+
+def handle_transfer(data):
+    origin = data.get('origin')
+    if origin is None:
+        return jsonify({'error': 'Origin account id is required'}), 400
+    
+    destination = data.get('destination')
+    if destination is None:
+        return jsonify({'error': 'Destination account id is required'}), 400
+    
+    amount = data.get('amount')
+    if amount is None:
+        return jsonify({'error': 'Amount is required'}), 400
+    
+    if accounts.get(origin) is None:
+        return jsonify(0), 404
+    
+    if accounts.get(destination) is None:
+        accounts[destination] = 0  # Create a new account if it doesn't exist
+    
+    accounts[destination] += amount
+    accounts[origin] -= amount
+
+    return jsonify({"origin": {"id":origin, "balance":accounts[origin]}, "destination": {"id":destination, "balance":accounts[destination]}}), 201
+
+
